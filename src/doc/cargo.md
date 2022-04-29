cf4983cf7e226708f8352361dff0f478b671a368
pystokes <ml4forecast@gmail.com>
2022-04-29 23:34:23 +0900

    Add docs

---
+++ src/doc/cargo.md
@@ -0,0 +1,52 @@
+# Cargo
+
+## Create new project
+
+```bash
+cargo new PROJECT_NAME --bin
+```
+
+## Build
+
+-> target/debug/BINARY
+
+```bash
+cargo build
+```
+
+## Build and run
+
+```bash
+cargo run
+```
+
+## Check (Not compile)
+
+Check compilable or not.  
+Run compile but not create binary.
+
+```bash
+cargo check
+```
+
+## Test
+
+```bash
+cargo test
+```
+
+## Build as release version
+
+Create optimized binary.
+
+```bash
+cargo build --release
+```
+
+## Update Cargo.lock
+
+Update `Cargo.lock`.
+
+```bash
+cargo update
+```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 
+#[get("/hello")]
+async fn hello_world() -> impl Responder {
+    format!("Hello World!")
+}
+
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    format!("active")
+}
+
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
+}
+
+#[get("/doc/func")]
+async fn func() -> impl Responder {
+    format!("func")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(hello_world)
+            .service(health_check)
             .service(greet)
-    })
+            .service(func)
+        })
     .bind(("0.0.0.0", 8000))?
     .run()
     .await
